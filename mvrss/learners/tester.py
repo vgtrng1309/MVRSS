Fix bug normalization in tester

mvrss/learners/tester.py
@@ -137,9 +137,9 @@ class Tester:
                     ra_outputs = ra_outputs.to(self.device)
 
                     if get_quali:
-                        quali_iter_rd = get_non_img_qualitatives(rd_outputs, rd_mask, self.paths,
+                        quali_iter_rd = get_qualitatives(rd_outputs, rd_mask, self.paths,
                                                          seq_name, quali_iter_rd, 'range_doppler')
-                        quali_iter_ra = get_non_img_qualitatives(ra_outputs, ra_mask, self.paths,
+                        quali_iter_ra = get_qualitatives(ra_outputs, ra_mask, self.paths,
                                                          seq_name, quali_iter_ra, 'range_angle')
 
                     rd_metrics.add_batch(torch.argmax(rd_mask, axis=1).cpu(),
@@ -252,9 +252,9 @@ class Tester:
                          [None, None, None],
                          [None, None, None]]
 
-        # fig = plt.figure()
-        # fig.tight_layout()
-        # ax = fig.subplots(2,4)
+        fig = plt.figure()
+        fig.tight_layout()
+        ax = fig.subplots(2)
         # fig2, ax2 = plt.subplots()
         # fig2.tight_layout()
         show_ragt = None
@@ -287,7 +287,7 @@ class Tester:
                 k = 0
                 for frame, rd, ra, ad in frame_dataloader:
                     k += 1
-                    if (k < 30):
+                    if (k < 50):
                         continue
                     rd_data = frame['rd_matrix'].to(self.device).float()
                     ra_data = frame['ra_matrix'].to(self.device).float()
@@ -297,15 +297,18 @@ class Tester:
                     rd_data_non_norm = rd_data.clone()
                     ra_data_non_norm = ra_data.clone()
                     ad_data_non_norm = ad_data.clone()
+
+                    # print(torch.max(ra_data), torch.min(ra_data))
+                    # print(torch.max(ad_data), torch.min(ad_data))
                     
                     # TODO: Change normalization here
                     ra_norm = normalize(ra, 'range_angle', norm_type=self.norm_type)
-                    print(torch.max(ra_norm), torch.min(ra_norm))
+                    # print(torch.max(ra_norm), torch.min(ra_norm))
                     rd_norm = normalize(rd, 'range_doppler', norm_type=self.norm_type)
                     ad_norm = normalize(ad, 'angle_doppler', norm_type=self.norm_type)
                     rd_data = normalize(rd_data, 'range_doppler', norm_type=self.norm_type)
                     ra_data = normalize(ra_data, 'range_angle', norm_type=self.norm_type)
-                    print(torch.max(ra_data), torch.min(ra_data))
+                    # print(torch.max(ra_data), torch.min(ra_data))
                     ad_data = normalize(ad_data, 'angle_doppler', norm_type=self.norm_type)
                     rd_mask = mask_to_img(torch.argmax(rd_mask, axis=1).cpu().numpy()[0])
                     ra_mask = mask_to_img(torch.argmax(ra_mask, axis=1).cpu().numpy()[0])
@@ -372,11 +375,17 @@ class Tester:
                     show_model_result = True
                     if (show_model_result):
                         start = time.time()
-                        if self.model == 'tmvanet':
-                            ad_data = normalize(ad_data, 'angle_doppler', norm_type=self.norm_type)
-                            rd_outputs, ra_outputs = net(rd_data, ra_data, ad_data)
-                        else:
+                        # if self.use_ad is True:
+                        #     ad_data = normalize(ad_data, 'angle_doppler', norm_type=self.norm_type)
+                            
+                        if self.model == 'mvnet' or self.model == 'radarformer2':
                             rd_outputs, ra_outputs = net(rd_data, ra_data)
+                        else:
+                            print(torch.max(rd_data), torch.min(rd_data))
+                            print(torch.max(ra_data), torch.min(ra_data))
+                            print(torch.max(ad_data), torch.min(ad_data))
+                            rd_outputs, ra_outputs = net(rd_data, ra_data, ad_data) 
+
                         end = time.time()
                         rd_outputs = rd_outputs.to(self.device)
                         ra_outputs = ra_outputs.to(self.device)
@@ -387,8 +396,8 @@ class Tester:
                         print("Model processing time: ", end - start)
                         #"""
                         if (start_frame):
-                            show_rapr = ax[0][3].imshow(ra_outputs)
-                            show_rdpr = ax[1][3].imshow(rd_outputs)
+                            show_rapr = ax[0].imshow(ra_outputs)
+                            show_rdpr = ax[1].imshow(rd_outputs)
                         else:
                             show_rapr.set_data(ra_outputs)
                             show_rdpr.set_data(rd_outputs)

mvrss/loaders/dataloaders.py
@@ -169,8 +169,8 @@ class CarradaDataset(Dataset):
         if np.random.uniform(0, 1) > 0.5:
             while (trans_angle == 0):
                 trans_angle=np.random.randint(-10,10,1)[0]
-        trans_range=-1
-        trans_angle=-1
+        # trans_range=-1
+        # trans_angle=-1
 
         rd_matrix = np.dstack(rd_matrices)
         rd_matrix = np.rollaxis(rd_matrix, axis=-1)
@@ -225,19 +225,21 @@ class CarradaDataset(Dataset):
             ra_frame['matrix'] = 10.0 * np.log10(ra_frame['matrix'])
             ad_frame['matrix'] = 10.0 * np.log10(ad_frame['matrix'])
         
+        data_stat = None
         if (self.norm_type == "tvt_raw"):
             data_stat = data_stat_raw
         elif (self.norm_type == "tvt_log"):
             data_stat = data_stat_log
 
-        rd_frame['matrix'][rd_frame['matrix'] > data_stat["rd_max_val"]] = data_stat["rd_max_val"]
-        rd_frame['matrix'][rd_frame['matrix'] < data_stat["rd_min_val"]] = data_stat["rd_min_val"]
-        
-        ra_frame['matrix'][ra_frame['matrix'] > data_stat["ra_max_val"]] = data_stat["ra_max_val"]
-        ra_frame['matrix'][ra_frame['matrix'] < data_stat["ra_min_val"]] = data_stat["ra_min_val"]
-        
-        ad_frame['matrix'][ad_frame['matrix'] > data_stat["ad_max_val"]] = data_stat["ad_max_val"]
-        ad_frame['matrix'][ad_frame['matrix'] < data_stat["ad_min_val"]] = data_stat["ad_min_val"]
+        if (data_stat is not None):
+            rd_frame['matrix'][rd_frame['matrix'] > data_stat["rd_max_val"]] = data_stat["rd_max_val"]
+            rd_frame['matrix'][rd_frame['matrix'] < data_stat["rd_min_val"]] = data_stat["rd_min_val"]
+            
+            ra_frame['matrix'][ra_frame['matrix'] > data_stat["ra_max_val"]] = data_stat["ra_max_val"]
+            ra_frame['matrix'][ra_frame['matrix'] < data_stat["ra_min_val"]] = data_stat["ra_min_val"]
+            
+            ad_frame['matrix'][ad_frame['matrix'] > data_stat["ad_max_val"]] = data_stat["ad_max_val"]
+            ad_frame['matrix'][ad_frame['matrix'] < data_stat["ad_min_val"]] = data_stat["ad_min_val"]
 
         frame = {'rd_matrix': rd_frame['matrix'], 'rd_mask': rd_frame['mask'],
                  'ra_matrix': ra_frame['matrix'], 'ra_mask': ra_frame['mask'],
@@ -524,7 +526,7 @@ class AngleShift():
         shape = matrix.shape
 
         # TODO: hard code here
-        shift_angle = -40
+        # shift_angle = -40
 
         # for i in range(matrix.shape[0]):
         #     matrix[i] = np.roll(matrix[i], shift_angle, 1)

mvrss/test.py
@@ -41,28 +41,26 @@ def test_model():
                       n_frames=cfg['nb_input_channels'])
 
     print('Number of trainable parameters in the model: %s' % str(count_params(model)))
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    if cfg['device'] == 'cuda':
-        model.cuda()
+    model.to(cfg['device'])
+    model.load_state_dict(torch.load(model_path,map_location=cfg['device']))
 
     tester = Tester(cfg)
     data = Carrada()
-    seq_name = "2019-09-16-13-18-33" # Two cars (summer - forward)
-    # seq_name = "2019-09-16-13-13-01"
+    # seq_name = "2019-09-16-13-18-33" # Two cars (summer - forward)
     # seq_name = "2019-09-16-13-13-01"
     # seq_name = "2019-09-16-13-20-20" # Two cars (summer - backward)
     # seq_name = "2020-02-28-13-06-53" # Cyclist and Car (winter - forward)
     # seq_name = "2020-02-28-13-10-51"
     # seq_name = "2020-02-28-12-23-30"
     test = data.get('Test')
-    tmp = test[seq_name]
-    test.clear()
-    test[seq_name] = tmp
+    #tmp = test[seq_name]
+    #test.clear()
+    #test[seq_name] = tmp
     testset = SequenceCarradaDataset(test)
     seq_testloader = DataLoader(testset, batch_size=1, shuffle=False, num_workers=0)
     tester.set_annot_type(cfg['annot_type'])
     
-    evaluate = False
+    evaluate = True
     if evaluate:
         if cfg['model'] == 'mvnet':
             test_results = tester.predict(model, seq_testloader, get_quali=True, add_temp=False)